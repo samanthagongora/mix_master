9ad4afbaa23093b96584103614f67c7ba358850b
Samantha Gongora <samantha.gongora@gmail.com>
2017-07-12 17:50:46 -0600

    adds feature test for creating new artist

--- Gemfile
+++ Gemfile
@@ -31,11 +31,15 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 gem 'unicorn'
+gem 'rails_12factor', group: :production
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'rspec-rails'
+  gem 'capybara'
+  gem 'launchy'
 end
 
 group :development do
---
+++ spec/features/user_creates_an_artist_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.feature "User submits a new artist" do
+  scenario "they enter data to create a new artist" do
+    image_path = 'http://cps-static.rovicorp.com/3/JPG_400/MI0003/146/MI0003146038.jpg'
+    name = 'Bob Marley'
+
+    visit '/artists'
+    click_on 'New Artist'
+    fill_in 'artist_name', with: name
+    fill_in 'artist_image_path', with: image_path
+    click_on 'Create Artist'
+
+    expect(page).to have_content name
+    expect(page).to have_css("img[src=\"#{image_path}\"]")
+  end
+end
