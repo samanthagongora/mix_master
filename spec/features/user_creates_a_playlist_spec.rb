b95f89345aea1234aa37dd2b48946c745562f50b
Samantha Gongora <samantha.gongora@gmail.com>
2017-07-13 08:35:54 -0600

    adds create playlist feature

--- spec/features/user_creates_a_playlist_spec.rb
+++ spec/features/user_creates_a_playlist_spec.rb
@@ -11,18 +11,18 @@ RSpec.feature "User creates a playlist" do
     visit '/playlists'
     click_on 'New Playlist'
     fill_in 'playlist_name', with: playlist_name
-    check("song-#{song_one.id}")
-    check("song-#{song_three.id}")
+    check("song-#{song1.id}")
+    check("song-#{song3.id}")
     click_on "Create Playlist"
 
     expect(page).to have_content playlist_name
 
     within("li:first") do
-      expect(page).to have_link song_one.title, href: song_path(song1)
+      expect(page).to have_link song1.title, href: song_path(song1)
     end
 
     within("li:last") do
-      expect(page).to have_link song_three.title, href: song_path(song3)
+      expect(page).to have_link song3.title, href: song_path(song3)
     end
   end
 end
